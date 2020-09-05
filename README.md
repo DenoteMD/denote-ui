c0d8026a9b9b250e78f0026405c454568ffbca6b
Chiro Hiro <chiro8x@gmail.com>
2020-09-05 20:03:37 +0700

    Implement first version of denoteui

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# denoteui
-Denote User Identify
+# Denote User Identify
+
+## Instruction
+
+We take care about your privacy, instead of username/password/email we offer you a new way to identify yourself without harm to your privacy.
+
+## Features
+
+- We using [BIP39](https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki) to help you store remember your key pair.
+- Your user's ID is digest of your public key
+- Message will be signed/verified by using ECDSA in this case we use `secp256k1`
+
+## Installation
+
+```
+npm i denoteui
+```
+
+## Example
+
+Create a new key pair by call `DenoteUserIdentity.createNewUser()` or recover from a mnemonic words `DenoteUserIdentity.fromMnemonic(...)`.
+
+```ts
+import DenoteUserIdentity from 'denoteui';
+const dui = new DenoteUserIdentity();
+console.log('Mnemonic:\t', dui.getMnemonic());
+console.log('User ID:\t', dui.getUserID());
+```
+
+Sign message:
+
+```ts
+const message = Buffer.from('Hello! this is example message');
+const signedMessage = dui.sign(message);
+console.log('Signed Message:\t', signedMessage.toString('base64'));
+```
+
+Recover user ID from signed message:
+
+```ts
+const recoveredUserID = DenoteUserIdentity.recoverUserID(signedMessage);
+console.log('Recovered ID:\t', recoveredUserID);
+const signedObj = dui.signObject({
+  name: 'Chiro Hiro',
+  age: 32,
+  data: 'Hello',
+});
+```
+
+Sign and recover object:
+
+```ts
+console.log('Singed object:\t', signedObj);
+console.log('Recovered object:\t', JSON.stringify(DenoteUserIdentity.recoverObject(signedObj)));
+```
+
+## License
+
+This software was licensed under [MIT License](https://github.com/DenoteMD/denoteui/blob/master/LICENSE)
---
+++ src/index.ts
@@ -0,0 +1,231 @@
+import bs58 from 'bs58';
+import { sha512, ripemd160, sha256 } from 'hash.js';
+import { ec as EC, curve } from 'elliptic';
+import { Buffer } from 'safe-buffer';
+import BN from 'bn.js';
+import * as bip39 from 'bip39';
+
+const prefix = 'Denote User Identity {00000000}:\n';
+const prefixLength = prefix.length;
+const rLength = 32;
+const sLength = 32;
+const rsLength = rLength + sLength;
+const vLength = 1;
+const jLength = 1;
+const sigLength = rLength + sLength + vLength + jLength;
+
+interface IRecoveredObject {
+  signer: string;
+  obj: object;
+}
+
+/**
+ * Denote User Identity
+ * @export
+ * @class DenoteUserIdentity
+ */
+export class DenoteUserIdentity {
+  /**
+   * Store mnemonic if possible
+   * @private
+   * @type {string}
+   * @memberof DenoteUserIdentity
+   */
+  private mnemonic: string = '';
+
+  /**
+   * Store key pair
+   * @private
+   * @type {EC.KeyPair}
+   * @memberof DenoteUserIdentity
+   */
+  private keyPair: EC.KeyPair;
+
+  /**
+   * Cache user id to saving computing power
+   * @private
+   * @type {string}
+   * @memberof DenoteUserIdentity
+   */
+  private cachedUserID: string = '';
+
+  /**
+   * Creates an instance of DenoteUserIdentity.
+   * @param {string} [mnemonic] If mnemonic was not set we will create a new one
+   * @memberof DenoteUserIdentity
+   */
+  constructor(mnemonic?: string) {
+    this.mnemonic = mnemonic || bip39.generateMnemonic();
+    const ec = new EC('secp256k1');
+    // New elliptic private key from sha512(entropy(mnemonic))
+    this.keyPair = ec.keyFromPrivate(
+      sha512().update(bip39.mnemonicToEntropy(this.mnemonic)).digest(),
+    );
+  }
+
+  /**
+   * Get mnemonic
+   * @return {*}  {string}
+   * @memberof DenoteUserIdentity
+   */
+  public getMnemonic(): string {
+    return this.mnemonic;
+  }
+
+  /**
+   * Sign and serialized message
+   * @param {Buffer} message
+   * @return {*}  {Buffer}
+   * @memberof DenoteUserIdentity
+   */
+  public sign(message: Buffer): Buffer {
+    const ec = new EC('secp256k1');
+    // Packed v,j in the same buffer
+    const vj = Buffer.alloc(2);
+    // Reserved uint64 for timestamp, please aware of year 2038 issue
+    const prefixedMessage = Buffer.allocUnsafe(message.length + prefixLength);
+    prefixedMessage.write(prefix, 0);
+    // Write timestamp to place holder, timestamp was used as nonce
+    // eslint-disable-next-line no-bitwise
+    prefixedMessage.writeUInt32BE(((Date.now() / 1000) & 0xffffffff) >>> 0, 26);
+    message.copy(prefixedMessage, prefixLength);
+    const messageDigest = sha256().update(prefixedMessage).digest();
+    const signature: EC.Signature = this.keyPair.sign(messageDigest);
+    const qPrime = new BN(
+      this.keyPair.getPublic().encode('hex', false),
+      'hex',
+      'be',
+    );
+    // Store v and j
+    vj.writeUInt8(signature.recoveryParam || 0, 0);
+    vj.writeUInt8(ec.getKeyRecoveryParam(undefined, signature, qPrime), 1);
+    // Serialized form
+    return Buffer.concat([
+      Buffer.from(signature.r.toArray('be')),
+      Buffer.from(signature.s.toArray('be')),
+      vj,
+      prefixedMessage,
+    ]);
+  }
+
+  /**
+   * Sign an object
+   * @param {*} obj
+   * @return {*}  {string}
+   * @memberof DenoteUserIdentity
+   */
+  public signObject(obj: object): string {
+    return this.sign(Buffer.from(JSON.stringify(obj))).toString('base64');
+  }
+
+  /**
+   * Get user ID of current key pair
+   * @return {*}
+   * @memberof DenoteUserIdentity
+   */
+  public getUserID() {
+    if (this.cachedUserID.length > 0) {
+      return this.cachedUserID;
+    }
+    if (typeof this.keyPair !== 'undefined') {
+      this.cachedUserID = DenoteUserIdentity.publicKeyToUserID(
+        this.keyPair.getPublic().encode('array', false),
+      );
+      return this.cachedUserID;
+    }
+    throw new Error('Key pair is undefined can not get ID');
+  }
+
+  /**
+   * Get UserID from public key
+   * base58(ripemd160(publicKey))
+   * @static
+   * @param {(string | number[])} pubKey
+   * @return {*}  {string}
+   * @memberof DenoteUserIdentity
+   */
+  public static publicKeyToUserID(pubKey: string | number[]): string {
+    let pubKeyDigest: number[];
+    if (typeof pubKey === 'string') {
+      pubKeyDigest = ripemd160().update(pubKey, 'hex').digest();
+    } else {
+      pubKeyDigest = ripemd160().update(pubKey).digest();
+    }
+    return bs58.encode(pubKeyDigest);
+  }
+
+  /**
+   * Recover user ID from signed message
+   * @static
+   * @param {Buffer} signedMessage
+   * @return {*}  {string}
+   * @memberof DenoteUserIdentity
+   */
+  public static recoverUserID(signedMessage: Buffer): string {
+    const ec = new EC('secp256k1');
+    const r = new BN(
+      signedMessage.slice(0, rLength).toString('hex'),
+      'hex',
+      'be',
+    );
+    const s = new BN(
+      signedMessage.slice(rLength, rsLength).toString('hex'),
+      'hex',
+      'be',
+    );
+    const recoveryParam = signedMessage.readUInt8(rsLength);
+    const j = signedMessage.readUInt8(rsLength + vLength);
+    const message = signedMessage.slice(sigLength);
+    const messageDigest = sha256().update(message).digest();
+    const pubKey = ec.recoverPubKey(
+      messageDigest,
+      {
+        r,
+        s,
+        recoveryParam,
+      },
+      j,
+    );
+    return DenoteUserIdentity.publicKeyToUserID(
+      (pubKey as curve.base.BasePoint).encode('hex', false),
+    );
+  }
+
+  /**
+   * Recover an object
+   * @static
+   * @param {string} signedObj
+   * @return {*}  {IRecoveredObject}
+   * @memberof DenoteUserIdentity
+   */
+  public static recoverObject(signedObj: string): IRecoveredObject {
+    const signedMessage = Buffer.from(signedObj, 'base64');
+    return {
+      obj: JSON.parse(signedMessage.slice(prefixLength + sigLength).toString()),
+      signer: DenoteUserIdentity.recoverUserID(signedMessage),
+    };
+  }
+
+  /**
+   * Create a new user
+   * @static
+   * @return {*}  {DenoteUserIdentity}
+   * @memberof DenoteUserIdentity
+   */
+  public static createNewUser(): DenoteUserIdentity {
+    return new DenoteUserIdentity(bip39.generateMnemonic());
+  }
+
+  /**
+   * Restore user from a given mnemonic
+   * @static
+   * @param {string} mnemonic
+   * @return {*}  {DenoteUserIdentity}
+   * @memberof DenoteUserIdentity
+   */
+  public static fromMnemonic(mnemonic: string): DenoteUserIdentity {
+    return new DenoteUserIdentity(mnemonic);
+  }
+}
+
+export default DenoteUserIdentity;
